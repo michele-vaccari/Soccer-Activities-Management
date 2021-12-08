f261291bd7b4b58ff9d2afebc09689c9e01e0b76
Michele Vaccari <michele.vaccari@outlook.com>
2021-12-08 17:33:13 +0100

    Update init.sql script

--- database/initialization-scripts/init.sql
+++ database/initialization-scripts/init.sql
@@ -1,9 +1,9 @@
 create table if not exists USERS.USER(
     ID INTEGER not null constraint USER_PK primary key,
+    "Type" VARCHAR(50) not null,
     "Name" VARCHAR(50) not null,
-    "Password" VARCHAR(50) not null,
     "Surname" VARCHAR(50) not null,
     "Email" VARCHAR(50) not null,
-    "Type" VARCHAR(50) not null,
+    "Password" VARCHAR(50) not null,
     "IsActive" BOOLEAN default TRUE not null
 );
