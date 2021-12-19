60406a6afb2177ea633803face2e27f956c8cb61
Michele Vaccari <michele.vaccari@outlook.com>
2021-12-19 12:06:53 +0100

    Add CORS configuration to web-api

--- web-api/src/main/java/com/sam/webapi/WebApiApplication.java
+++ web-api/src/main/java/com/sam/webapi/WebApiApplication.java
@@ -1,13 +1,37 @@
 package com.sam.webapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebApiApplication {
 
+	@Autowired
+	private Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				var corsRegistration = registry.addMapping("/**");
+
+				var allowedOrigins = env.getProperty("cors.allowedOrigins");
+				for (var allowedOrigin : allowedOrigins.split(","))
+					corsRegistration.allowedOrigins(allowedOrigin);
+
+				corsRegistration.allowedMethods(env.getProperty("cors.allowedMethods"));
+			}
+		};
 	}
 
 }
