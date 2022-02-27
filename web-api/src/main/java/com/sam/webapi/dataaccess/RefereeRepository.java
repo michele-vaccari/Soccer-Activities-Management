9df8ccb620314dd3a174809d89036cb7c4ab17d5
Michele Vaccari <michele.vaccari@outlook.com>
2022-02-27 15:58:03 +0100

    Fix RefereeRepository

--- web-api/src/main/java/com/sam/webapi/dataaccess/RefereeRepository.java
+++ web-api/src/main/java/com/sam/webapi/dataaccess/RefereeRepository.java
@@ -1,12 +1,7 @@
 package com.sam.webapi.dataaccess;
 
 import com.sam.webapi.model.Referee;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RefereeRepository extends CrudRepository<Referee, Integer> {
-	@Modifying
-	@Query(value = "SELECT * FROM SAM.USER AS U, SAM.REGISTERED_USER AS RU, SAM.REFEREE AS R WHERE U.ACTIVE = 'Y' AND U.ID = RU.ID AND RU.ID = R.ID", nativeQuery=true)
-	Iterable<Referee> findAllActive();
 }
