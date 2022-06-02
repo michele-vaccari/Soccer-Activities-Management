1c2101c0d01deb9c8729f1ea52e996c7a7e97348
Michele Vaccari <michele.vaccari@outlook.com>
2022-06-03 01:21:43 +0200

    Demo: Update init.sql

--- database/initialization-scripts/createschema.sh
+++ database/initialization-scripts/createschema.sh
@@ -14,9 +14,9 @@ export sql_init_script_path=/var/custom/init.sql
 echo "Run $sql_init_script"
 ./db2 -tvf $sql_init_script_path
 
-echo "Add default admin user"
-export sql_add_default_admin_user_script_path=/var/custom/add_default_admin_user.sql
-./db2 -tvf $sql_add_default_admin_user_script_path
+#echo "Add default admin user"
+#export sql_add_default_admin_user_script_path=/var/custom/add_default_admin_user.sql
+#./db2 -tvf $sql_add_default_admin_user_script_path
 
 echo "Remove all script"
 rm /var/custom/*
