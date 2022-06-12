fa5cc8e55237472465f69a5bac6a447510bd2aae
Michele Vaccari <michele.vaccari@outlook.com>
2022-06-12 23:47:54 +0200

    Database: add the ability to use demo data

--- database/initialization-scripts/createschema.sh
+++ database/initialization-scripts/createschema.sh
@@ -10,13 +10,19 @@ echo "Connect to $DBNAME database"
 cd /home/db2inst1/sqllib/bin/
 ./db2 connect to $DBNAME
 
-export sql_init_script_path=/var/custom/init.sql
+sql_init_script_path=/var/custom/init.sql
 echo "Run $sql_init_script"
 ./db2 -tvf $sql_init_script_path
 
-#echo "Add default admin user"
-#export sql_add_default_admin_user_script_path=/var/custom/add_default_admin_user.sql
-#./db2 -tvf $sql_add_default_admin_user_script_path
+if [ "$USE_DEMO_DATA" = true ] ; then
+    echo "Add demo data"
+    sql_demo_data_script_path=/var/custom/demo_data.sql
+    ./db2 -tvf $sql_demo_data_script_path
+else
+    echo "Add default admin user"
+    sql_add_default_admin_user_script_path=/var/custom/add_default_admin_user.sql
+    ./db2 -tvf $sql_add_default_admin_user_script_path
+fi
 
 echo "Remove all script"
 rm /var/custom/*
